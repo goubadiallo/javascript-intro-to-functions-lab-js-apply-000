bcf33e853d905154fff718aa600ff6c3c2f4322f
Mamadou Diallo <mamadou.diallo@wework.com>
2018-10-24 23:14:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+}
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  
+console.log('HELLO')
+}
+function logWhisper(string){
+  console.log('hello')
+}
+function sayHiToGrandma(string){
+  return "I can't hear you!"
+}
+return string.toUpperCase()
+return 'YES INDEED!'
